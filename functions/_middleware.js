6d28032148ddab14731d71bf30aed347f4a62e2d
roxandvasile <roxanaandreeavasile@gmail.com>
2026-01-02 22:44:31 +0200

    last update for url

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,16 +2,6 @@ export function onRequest({ request }) {
   const url = new URL(request.url);
 
   if (url.hostname.endsWith(".pages.dev")) {
-    return new Response("Page Not Found", { status: 404 });
-  }
-
-  return fetch(request);
-}
-
-export function onRequest({ request }) {
-  const url = new URL(request.url);
-
-  if (url.hostname.endsWith(".pages.dev")) {
     url.hostname = "cabinetrecuperare.ro";
     return Response.redirect(url.toString(), 301);
   }
